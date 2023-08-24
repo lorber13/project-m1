020a78dd51bf810ca3c5ddcf25eb7b02d6c50d6d
lorber13 <lorenzo.bertetto@gmail.com>
2023-08-24 10:44:46 +0200

    Separazione tra wayland e le altre piattaforme.

--- global_key_activation/src/main.rs
+++ global_key_activation/src/main.rs
@@ -1,63 +1,106 @@
-use std::thread;
+use std::{env, thread};
 use eframe::egui;
 use global_hotkey::{hotkey::HotKey, GlobalHotKeyEvent, GlobalHotKeyManager};
 use global_hotkey::hotkey::{Code, Modifiers};
 
+
 fn main() -> Result<(), eframe::Error> {
-    let manager = GlobalHotKeyManager::new().unwrap();
-    let hotkey = HotKey::new(Some(Modifiers::SHIFT), Code::KeyD);
+    let wayland = env::var("WAYLAND_DISPLAY").is_ok();
 
-    manager.register(hotkey).unwrap();
+    let options = eframe::NativeOptions::default();
 
-    let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
-        ..Default::default()
-    };
-
-    // event loop, listening for the hotkeys
-    thread::spawn(|| {
-        loop {
-            if let Ok(event) = GlobalHotKeyEvent::receiver().recv() {
-                println!("tray event: {event:?}");
+    if wayland {
+        eframe::run_native(
+            "My egui App",
+            options,
+            Box::new(|_cc| Box::<WaylandContent>::new(WaylandContent::new())),
+        )
+    } else {
+        // event loop, listening for the hotkeys
+        thread::spawn(|| {
+            loop {
+                if let Ok(event) = GlobalHotKeyEvent::receiver().recv() {
+                    println!("tray event: {event:?}");
+                }
             }
-        }
-    });
+        });
 
-    eframe::run_native(
-        "My egui App",
-        options,
-        Box::new(|_cc| Box::<MyApp>::default()),
-    )
+        eframe::run_native(
+            "My egui App",
+            options,
+            Box::new(|_cc| Box::<DefaultContent>::new(DefaultContent::new())),
+        )
+    }
+}
+struct WaylandContent {
 }
 
-struct MyApp {
-    name: String,
-    age: u32,
+impl WaylandContent {
+    fn new() -> Self {
+        todo!()
+    }
 }
 
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
+impl eframe::App for WaylandContent {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            todo!()
+        });
+    }
+}
+
+struct DefaultContent {
+    manager: GlobalHotKeyManager,
+    hotkey: HotKey,
+    modifier: Modifiers,
+    key: Code
+}
+
+impl DefaultContent {
+    fn new() -> Self {
+        let manager = GlobalHotKeyManager::new().unwrap();
+        let hotkey = HotKey::new(Some(Modifiers::SHIFT), Code::KeyD);
+        manager.register(hotkey).unwrap();
+        DefaultContent {
+            manager,
+            hotkey,
+            modifier: Default::default(),
+            key: Default::default(),
         }
     }
 }
 
-impl eframe::App for MyApp {
+impl Default for DefaultContent {
+    fn default() -> Self {
+        Self {
+            manager: GlobalHotKeyManager::new().unwrap(),
+            hotkey: HotKey::new(Some(Modifiers::SHIFT), Code::KeyD),
+            modifier: Modifiers::SHIFT,
+            key: Code::KeyD
+        }
+    }
+}
+
+impl eframe::App for DefaultContent {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
             ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
+                ui.radio_value(&mut self.key, Code::Digit0, "0");
+                ui.radio_value(&mut self.key, Code::Digit1, "1");
+                ui.radio_value(&mut self.key, Code::Digit2, "2");
+                ui.radio_value(&mut self.key, Code::Digit3, "3");
+                ui.radio_value(&mut self.key, Code::Digit4, "4");
+                ui.radio_value(&mut self.key, Code::Digit5, "5");
+                ui.radio_value(&mut self.key, Code::Digit6, "6");
+                ui.radio_value(&mut self.key, Code::Digit7, "7");
+                ui.radio_value(&mut self.key, Code::Digit8, "8");
+                ui.radio_value(&mut self.key, Code::Digit9, "9");
             });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
+            if ui.button("set").clicked() {
+                self.manager.unregister(self.hotkey).unwrap();
+                self.hotkey = HotKey::new(None, self.key);
+                self.manager.register(self.hotkey).unwrap();
             }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
         });
     }
 }
