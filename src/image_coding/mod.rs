602e73a0276d6fe09baa3264d832eb3f14e229aa
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2023-08-29 21:59:19 +0200

    sostituzione metodi per ogni diverso formato con un unico metodo
    funziona solo con la versione 0.8.2 del crate screenshots (e successive) 
    
    Introdotto anche un metodo "available_formats()" che ritorna un Vec<&'static str> con l'elenco dei nomi di tutti i formati disponibili (attualment solo tre, ma si può valutare di aggiungerne altri)

--- src/image_coding/mod.rs
+++ src/image_coding/mod.rs
@@ -1,4 +1,4 @@
-use image::{RgbaImage, ImageEncoder};
+use image::{RgbaImage, ImageEncoder, ImageError};
 use arboard::{Clipboard, ImageData};
 use image::codecs::{jpeg::JpegEncoder, gif::GifEncoder};
 use std::fs::*;
@@ -10,6 +10,28 @@ pub enum ImageFormat { //Enum per selezione del formato
     JPEG,
     GIF,
 }
+
+impl Into<&str> for ImageFormat
+{
+    fn into(self) -> &'static str
+    {
+        match self
+        {
+            Self::Png => "Png",
+            Self::JPEG => "Jpeg",
+            Self::GIF => "Gif"
+        }
+    }
+}
+
+impl ImageFormat
+{
+    pub fn available_formats() -> Vec<&'static str>
+{
+    vec![ImageFormat::Png.into(), ImageFormat::JPEG.into(), ImageFormat::GIF.into()]
+} 
+}
+
 #[derive(Debug, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub enum ScreenshotDim{ //Enum per la scelta del tipo di screenshot
@@ -26,20 +48,17 @@ pub fn copy_to_clipboard(img: &RgbaImage) -> Result<(), arboard::Error>
 
 
 
-pub fn save_in_png(file_output: File, img: RgbaImage) -> image::ImageResult<()>
+pub fn save_image(file_output: &std::path::Path, img: RgbaImage) -> image::ImageResult<()>
 {
-    let pnge = image::codecs::png::PngEncoder::new(file_output);
-    return pnge.write_image(&img, img.width(), img.height(), image::ColorType::Rgba8);  
-}
-
-pub fn save_in_jpeg (file_output: File, img: RgbaImage) -> image::ImageResult<()>
-{   //salvataggio in jpeg senza passare da png, usando Encoder fornito dal crate image
-    let mut encoder = JpegEncoder::new(file_output);
-    encoder.encode(&img, img.width(), img.height(), image::ColorType::Rgba8)
-}
-
-pub fn save_in_gif (file_output: File, img: RgbaImage) -> image::ImageResult<()>
-{
-    let mut encoder = GifEncoder::new(file_output);  
-    encoder.encode(&img, img.width() , img.height(), image::ColorType::Rgba8)
+    if let Some(ext) = file_output.extension()
+    {
+        if ImageFormat::available_formats().contains(ext)
+        {
+            return img.save(file_output);
+        }
+        return Err(ImageError::IoError(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Invalid file extension")));
+    }
+    
+    return Err(ImageError::IoError(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Missing file extension")));
+      
 }
