05e64dd91cf3bd6a6a4735091676e10fc6b2508b
lorber13 <lorenzo.bertetto@gmail.com>
2023-08-29 10:19:40 +0200

    Lo stato "capturing" non serve piu'. Codice riformattato dopo il merge.

--- src/gui/rect_selection.rs
+++ src/gui/rect_selection.rs
@@ -1,78 +1,78 @@
+use super::{main_window::MainWindow, EnumGuiState};
 use eframe::egui;
-use egui::{pos2, Color32, ColorImage, Pos2, Rect, Rounding, Sense, Stroke, Vec2, CentralPanel, Key};
+use egui::{pos2, Color32, ColorImage, Pos2, Rect, Rounding, Sense, Stroke, Vec2};
 use egui_extras::RetainedImage;
 use screenshots::Screen;
-use super::{EnumGuiState, main_window::MainWindow};
-use std::{rc::Rc, cell::RefCell};
+use std::{cell::RefCell, rc::Rc};
 
 pub struct RectSelection {
     image: RetainedImage,
     start_drag_point: Option<Pos2>,
-    global_gui_state: Rc<RefCell<EnumGuiState>>
+    global_gui_state: Rc<RefCell<EnumGuiState>>,
 }
 
 impl RectSelection {
     pub fn new(global_gui_state: Rc<RefCell<EnumGuiState>>) -> Self {
         Self {
-            image:  capture_screenshot(),
+            image: capture_screenshot(),
             start_drag_point: None,
-            global_gui_state
+            global_gui_state,
         }
     }
 }
 
 impl eframe::App for RectSelection {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-            frame.set_fullscreen(true);
-            egui::Area::new("area_1").show(ctx, |ui| {
-                let (space, painter) = ui.allocate_painter(
-                    Vec2::new(ctx.screen_rect().width(), ctx.screen_rect().height()),
-                    Sense::click_and_drag(),
-                );
-                painter.image(
-                    self.image.texture_id(ctx),
-                    Rect::from_min_max(
-                        pos2(0.0, 0.0),
-                        pos2(ctx.screen_rect().width(), ctx.screen_rect().height()),
-                    ),
-                    Rect::from_min_max(pos2(0.0, 0.0), pos2(1.0, 1.0)),
-                    Color32::from_white_alpha(30),
-                );
+        frame.set_fullscreen(true);
+        egui::Area::new("area_1").show(ctx, |ui| {
+            let (space, painter) = ui.allocate_painter(
+                Vec2::new(ctx.screen_rect().width(), ctx.screen_rect().height()),
+                Sense::click_and_drag(),
+            );
+            painter.image(
+                self.image.texture_id(ctx),
+                Rect::from_min_max(
+                    pos2(0.0, 0.0),
+                    pos2(ctx.screen_rect().width(), ctx.screen_rect().height()),
+                ),
+                Rect::from_min_max(pos2(0.0, 0.0), pos2(1.0, 1.0)),
+                Color32::from_white_alpha(30),
+            );
 
-                if !space.clicked() {
-                    match (space.drag_started(), space.drag_released()) {
-                        (true, false) => {
-                            self.start_drag_point = space.hover_pos().map(|point| point.round());
-                        }
-                        (false, true) => {
-                            println!("Screenshot has to be saved");
-                            self.global_gui_state.replace(EnumGuiState::ShowingMainWindow(Rc::new(RefCell::new(MainWindow::new(self.global_gui_state.clone())))));
-                        }
-                        (false, false) => {
-                            if let Some(pos1) = self.start_drag_point {
-                                painter.rect(
-                                    Rect::from_points(&[
-                                        pos1,
-                                        space
-                                            .hover_pos()
-                                            .map(|point| point.round())
-                                            .expect("error"),
-                                    ]),
-                                    Rounding::none(),
-                                    Color32::from_white_alpha(30),
-                                    Stroke::NONE,
-                                )
-                            }
-                        }
-                        _ => {}
+            if !space.clicked() {
+                match (space.drag_started(), space.drag_released()) {
+                    (true, false) => {
+                        self.start_drag_point = space.hover_pos().map(|point| point.round());
                     }
-                } else {
-                    // line put to prevent a strange bug in case of a click todo: investigate
-                    self.start_drag_point = None;
+                    (false, true) => {
+                        println!("Screenshot has to be saved");
+                        self.global_gui_state
+                            .replace(EnumGuiState::ShowingMainWindow(Rc::new(RefCell::new(
+                                MainWindow::new(self.global_gui_state.clone()),
+                            ))));
+                    }
+                    (false, false) => {
+                        if let Some(pos1) = self.start_drag_point {
+                            painter.rect(
+                                Rect::from_points(&[
+                                    pos1,
+                                    space.hover_pos().map(|point| point.round()).expect("error"),
+                                ]),
+                                Rounding::none(),
+                                Color32::from_white_alpha(30),
+                                Stroke::NONE,
+                            )
+                        }
+                    }
+                    _ => {}
                 }
+            } else {
+                // line put to prevent a strange bug in case of a click todo: investigate
+                self.start_drag_point = None;
+            }
 
-                frame.set_visible(true);
-            });
+            frame.set_visible(true);
+        });
     }
 }
 
