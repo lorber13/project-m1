15428dd1c3af347718cb01f83667c901f6846ea0
lorber13 <lorenzo.bertetto@gmail.com>
2023-09-10 17:12:23 +0200

    Checkpoint che non compila

--- src/gui/rect_selection.rs
+++ src/gui/rect_selection.rs
@@ -8,6 +8,7 @@ use std::sync::{Arc, Mutex};
 use std::io::stderr;
 use std::io::Write;
 use std::sync::mpsc::Sender;
+use image::RgbaImage;
 
 pub struct RectSelection {
     image: RetainedImage,
@@ -88,58 +89,5 @@ impl RectSelection {
     }
 }
 
-impl eframe::App for RectSelection {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        frame.set_fullscreen(true);
-        
-        egui::Area::new("area_1").show(ctx, |ui| {
-            let (space, painter) = ui.allocate_painter(
-                Vec2::new(ctx.screen_rect().width(), ctx.screen_rect().height()),
-                Sense::click_and_drag(),
-            );
-            painter.image(
-                self.image.texture_id(ctx),
-                Rect::from_min_max(
-                    pos2(0.0, 0.0),
-                    pos2(ctx.screen_rect().width(), ctx.screen_rect().height()),
-                ),
-                Rect::from_min_max(pos2(0.0, 0.0), pos2(1.0, 1.0)),
-                Color32::from_white_alpha(30),
-            );
 
-            if !space.clicked() {
-                match (space.drag_started(), space.drag_released()) {
-                    (true, false) => {
-                        self.start_drag_point = space.hover_pos().map(|point| point.round());
-                    }
-                    (false, true) => {
-                        self.send_rect_selected(Rect::from_points(&[
-                                                                    self.start_drag_point.unwrap(),
-                                                                    space.hover_pos().map(|point| point.round()).expect("error"),
-                                                                ])); 
-                    }
-                    (false, false) => {
-                        if let Some(pos1) = self.start_drag_point {
-                            painter.rect(
-                                Rect::from_points(&[
-                                    pos1,
-                                    space.hover_pos().map(|point| point.round()).expect("error"),
-                                ]),
-                                Rounding::none(),
-                                Color32::from_white_alpha(30),
-                                Stroke::NONE,
-                            )
-                        }
-                    }
-                    _ => {}
-                }
-            } else {
-                // line put to prevent a strange bug in case of a click todo: investigate
-                self.start_drag_point = None;
-            }
-
-            frame.set_visible(true);
-        });
-    }
-}
 
