ae2626289480ec17d017cae8dd4e6c7f1722c0f6
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2023-09-15 16:18:14 +0200

    Delete gui directory

--- gui/loading.rs
+++
@@ -1,8 +0,0 @@
-use eframe::egui::CentralPanel;
-use crate::gui::egui::Context;
-
-pub(crate) fn show_loading(ctx: &Context) {
-    CentralPanel::default().show(ctx, |ui| {
-        ui.centered_and_justified(|ui2| ui2.spinner());
-    });
-}
