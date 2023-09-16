8317f5377008c17b1e66a058bfde425354b8fda5
lorber13 <lorenzo.bertetto@gmail.com>
2023-09-16 11:44:04 +0200

    Commit con visualizzazione scalata dell'immagine.

--- src/gui/edit_image.rs
+++ src/gui/edit_image.rs
@@ -1,7 +1,10 @@
-use crate::image_coding::ImageFormat;
-use eframe::egui::{CentralPanel, ColorImage, Context, Sense, TextureHandle, ScrollArea, Vec2};
-use image::RgbaImage;
 use super::egui::ComboBox;
+use crate::image_coding::ImageFormat;
+use eframe::egui::{
+    pos2, vec2, CentralPanel, Color32, ColorImage, Context, Painter, Rect, Response, Sense,
+    TextureHandle, Ui,
+};
+use image::RgbaImage;
 
 pub struct EditImage {
     image: RgbaImage,
@@ -13,7 +16,7 @@ pub enum EditImageEvent {
     Saved {
         image: RgbaImage,
         format: ImageFormat,
-    }, // todo: add image object to be returned
+    },
     Aborted,
     Nil,
 }
@@ -30,8 +33,35 @@ impl EditImage {
                 Default::default(),
             ),
             image: rgba,
-            format: ImageFormat::Png
+            format: ImageFormat::Png,
         }
+    }
+    fn display_image(&mut self, ui: &mut Ui) -> (Response, Painter) {
+        let available_size = ui.available_size_before_wrap();
+        let image_size = self.texture_handle.size_vec2();
+        let scaling_ratio = {
+            let mut ratio = if image_size.x / available_size.x > image_size.y / available_size.y {
+                available_size.x / image_size.x
+            } else {
+                available_size.y / image_size.y
+            };
+            if ratio > 1.0 {
+                ratio = 1.0;
+            }
+            ratio
+        };
+        let scaled_dimensions = vec2(
+            self.texture_handle.size()[0] as f32 * scaling_ratio,
+            self.texture_handle.size()[1] as f32 * scaling_ratio,
+        );
+        let (response, painter) = ui.allocate_painter(scaled_dimensions, Sense::click_and_drag());
+        painter.image(
+            self.texture_handle.id(),
+            Rect::from_min_size(painter.clip_rect().left_top(), scaled_dimensions),
+            Rect::from_min_max(pos2(0.0, 0.0), pos2(1.0, 1.0)),
+            Color32::WHITE,
+        );
+        (response, painter)
     }
     pub fn update(
         &mut self,
@@ -40,14 +70,12 @@ impl EditImage {
         enabled: bool,
     ) -> EditImageEvent {
         let mut ret = EditImageEvent::Nil;
-
         CentralPanel::default().show(ctx, |ui| {
-                ui.add_enabled_ui(enabled, |ui| {
-                    ui.horizontal_top(|ui| {
-
-                        ComboBox::from_label("") //menù a tendina per la scelta del formato di output
-                        .selected_text(format!("{:?}", self.format ))
-                        .show_ui(ui, |ui|{
+            ui.add_enabled_ui(enabled, |ui| {
+                ui.horizontal_top(|ui| {
+                    ComboBox::from_label("") //menù a tendina per la scelta del formato di output
+                        .selected_text(format!("{:?}", self.format))
+                        .show_ui(ui, |ui| {
                             ui.style_mut().wrap = Some(false);
                             ui.set_min_width(60.0);
                             ui.selectable_value(&mut self.format, ImageFormat::Png, "Png");
@@ -55,28 +83,21 @@ impl EditImage {
                             ui.selectable_value(&mut self.format, ImageFormat::GIF, "Gif");
                         });
 
+                    if ui.button("Save").clicked() {
+                        ret = EditImageEvent::Saved {
+                            image: self.image.clone(),   // todo: ugly clone
+                            format: self.format.clone(), // todo: should be a state
+                        };
+                    }
 
-                        if ui.button("Save").clicked() {
-                            ret = EditImageEvent::Saved {
-                                image: self.image.clone(), // todo: ugly clone
-                                format: self.format.clone(),  // todo: should be a state
-                            };
-                        }
-                        
-
-                        if ui.button("Abort").clicked() {
-                            ret = EditImageEvent::Aborted;
-                        }
-                        ui.set_max_height(30.0);
-                    });
-                    ui.end_row();
-
-                    ScrollArea::both().show(ui, |ui|{
-                        let (response, painter) =
-                            ui.allocate_painter(Vec2::from([0.0,0.0]), Sense::click_and_drag());
-                        ui.image(self.texture_handle.id(), self.texture_handle.size_vec2());
-                    });
+                    if ui.button("Abort").clicked() {
+                        ret = EditImageEvent::Aborted;
+                    }
+                    ui.set_max_height(30.0);
                 });
+                ui.end_row();
+                let (response, painter) = self.display_image(ui);
+            });
         });
         ret
     }
